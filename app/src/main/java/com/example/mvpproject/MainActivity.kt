c4696ecc586dea3b05d856f37701ef992031457c
catrin-crypto <78826785+catrin-crypto@users.noreply.github.com>
2021-11-06 00:10:54 +0300

    Lesson 1

--- app/src/main/java/com/example/mvpproject/MainActivity.kt
+++ app/src/main/java/com/example/mvpproject/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.mvpproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.mvpproject.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
+    private var viewBinding: ActivityMainBinding? = null
+    val presenter = MainPresenter(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding?.root)
+
+        viewBinding?.button1?.setOnClickListener { presenter.counterClick(0) }
+        viewBinding?.button2?.setOnClickListener { presenter.counterClick(1) }
+        viewBinding?.button3?.setOnClickListener { presenter.counterClick(2) }
+    }
+
+    override fun setButtonText(index: Int, text: String) {
+        when (index) {
+            0 -> viewBinding?.button1?.text = text
+            1 -> viewBinding?.button2?.text = text
+            2 -> viewBinding?.button3?.text = text
+        }
     }
 }
---
+++ app/src/main/java/com/example/mvpproject/MainView.kt
@@ -0,0 +1,5 @@
+package com.example.mvpproject
+
+interface MainView {
+    fun setButtonText(index : Int, text : String)
+}
